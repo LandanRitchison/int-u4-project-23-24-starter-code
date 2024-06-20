a2676907ad6777b335e55f21c91dffa83f35d2fd
LandanRitchison <Ritchisonlandan@gmail.com>
2024-06-20 12:59:23 -0400

    UserGame v1

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // JavaScript goes here.
+let UserTextOne = document.querySelector("#userResponseOne");
+let UserTextTwo = document.querySelector("#userResponseTwo");
+let submitButton = document.querySelector(".submitButton");
+let Feedback = document.querySelector(".result");
+// console logs all variables
+console.log(UserTextOne);
+console.log(UserTextTwo);
+console.log(submitButton);
+console.log(Feedback);
+
+submitButton.addEventListener("click", function(){
+    let AgeValue = parseInt(UserTextOne.value);
+    let RunFar = UserTextTwo.value;
+
+if (AgeValue <= 40 && RunFar === "yes" ) {
+    Feedback.innerHTML = "You Should Try Football";
+}else if (AgeValue <= 40 && RunFar === "no"){
+  Feedback.innerHTML = "You Should Try Basketball";
+} else if (AgeValue >= 40 && RunFar === "yes") {
+    Feedback.innerHTML = "You Should Try Cross Country";
+} else if (AgeValue >= 40 && RunFar === "no") {
+    Feedback.innerHTML = "You Should Try a relaxed sport";
+} else {
+    Feedback.innerHTML = "Enter a valid answer"
+}
+
+});
